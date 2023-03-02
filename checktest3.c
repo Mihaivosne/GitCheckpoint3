2e68124ee53fc49dc37486f851ec86f1e6989953
Mihaj-Alexandru Vosnesensky <mihaj-alexandru.vosnesensky@brightstraining.com>
2023-03-02 15:01:20 +0100

    Update checktest3.c

--- checktest3.c
+++ checktest3.c
@@ -1,1 +1,1 @@
-#include<stdio.h>\nint main(){int i;for(i=1;i<=10;i++)printf("%d\n",i);return 0;}
+#include<stdio.h>\nint main(){int i;for(i=1;i<=10;i++)printf("%d\n",i);printf("Task 3 checked\n");return 0;}
